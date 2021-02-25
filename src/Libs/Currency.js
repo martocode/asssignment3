4e4ff4c9aeb066ff51d1a1942e0ea76e515d476c
martocode <martocode@gmail.com>
2021-02-25 19:55:25 +0700

    changes

--- src/Libs/Currency.js
+++ src/Libs/Currency.js
@@ -1,43 +1,20 @@
 export const convert = (getRates, base, select) => (to_currency, getInput) => {
 	return (n) => {
-		let result;
-		const formater = currFormat(select);
-		let currNum = new Promise((e) => e())
-			// .then(() => console.log(getInput, "x before", to_currency))
-			.then(() => {
-				return (getInput / getRates[select]) * getRates[to_currency];
-			});
-		/* .then((curr) => {
-				console.log(curr, "x", to_currency);
-				return curr;
-			}); */
-		// let result;
+		let notBase = true;
+		getInput /= getRates[select];
 
-		let test = (async () => {
-			let curr = await currNum;
-			// console.log(curr, "curr");
-			/* if (n === 0) {
-				// console.log(curr, "0 before", to_currency);
-				result = curr * 1.02;
-				// console.log(getInput);
-			} else if (n === 1) {
-				result = curr * 0.98;
-			} else if (n === undefined) {
-				result = curr;
-			} */
-			// result = curr;
-			return curr;
-		})(n);
-		// if (result) {
-		// console.log(formater(test), "result");
-		// return result;
-		// }
+		if (to_currency !== base) {
+			console.log(getInput, " before", to_currency);
+			getInput *= getRates[to_currency];
+			notBase = false;
+		}
+		if (n === 0) {
+			getInput *= 1.02;
+		} else if (n === 1) {
+			getInput *= 0.98;
+		} else if (n === undefined) {
+		}
+
+		return getInput;
 	};
 };
-
-export const currFormat = (select) => (number) => {
-	return Intl.NumberFormat({ style: "currency", currency: select }).format(
-		number
-	);
-};
-// export default convert;
